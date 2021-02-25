f692d846f3bd023f26ff4eabf4f5af11c18fa3a8
Alexandr Novichkov <alexandr.novichkov@ncloudtech.ru>
2021-02-26 01:31:56 +0300

    T-30 | feat: add cors

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/deckarep/golang-set v1.7.1
 	github.com/go-kit/kit v0.10.0
 	github.com/gogo/protobuf v1.3.2
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
 	github.com/golang/protobuf v1.4.3
 	github.com/golang/snappy v0.0.3 // indirect
 	github.com/grpc-ecosystem/grpc-gateway v1.16.0
--- server/frontend/reverse_proxy.go
+++ server/frontend/reverse_proxy.go
@@ -3,7 +3,9 @@ package frontend
 import (
 	"context"
 	"net/http"
+	"strings"
 
+	"github.com/golang/glog"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"google.golang.org/grpc"
 
@@ -12,6 +14,36 @@ import (
 
 func serveSwagger(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "www/swagger.json")
+}
+
+func preflightHandler(w http.ResponseWriter, r *http.Request) {
+	headers := []string{"Content-Type", "Accept"}
+	w.Header().Set("Access-Control-Allow-Headers", strings.Join(headers, ","))
+
+	methods := []string{"GET", "HEAD", "POST", "PUT", "DELETE"}
+	w.Header().Set("Access-Control-Allow-Methods", strings.Join(methods, ","))
+
+	glog.Infof("preflight request for %s", r.URL.Path)
+
+	return
+}
+
+// allowCORS allows Cross Origin Resoruce Sharing from any origin.
+// Don't do this without consideration in production systems.
+func allowCORS(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if origin := r.Header.Get("Origin"); origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+
+			if r.Method == "OPTIONS" && r.Header.Get("Access-Control-Request-Method") != "" {
+				preflightHandler(w, r)
+
+				return
+			}
+		}
+
+		h.ServeHTTP(w, r)
+	})
 }
 
 // StartReverseProxy start reverse proxy for front
@@ -36,5 +68,5 @@ func StartReverseProxy(backendEndpoint, frontendEndpoint string) error {
 	mux.Handle("/swagger-ui/", http.StripPrefix("/swagger-ui", fs))
 
 	// start HTTP server (and proxy calls to gRPC server endpoint)
-	return http.ListenAndServe(frontendEndpoint, mux)
+	return http.ListenAndServe(frontendEndpoint, allowCORS(mux))
 }
