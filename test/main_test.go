edcc279f93144aee7cd82e063e2005d2845a166d
Alexandr Novichkov <alexandr.novichkov@ncloudtech.ru>
2021-02-26 03:03:09 +0300

    T-30 | feat: write tests

--- test/main_test.go
+++ test/main_test.go
@@ -3,27 +3,32 @@ package test
 import (
 	"context"
 	"go/build"
-	"io/ioutil"
-	"log"
+	"io"
 	"net/http"
+	"net/url"
+	"path"
 	"path/filepath"
 	"testing"
 	"time"
 
+	"github.com/golang/protobuf/jsonpb"
+	"github.com/golang/protobuf/proto"
 	"github.com/golang/protobuf/ptypes"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/memprofiler/memprofiler/schema"
+	"github.com/memprofiler/memprofiler/server/config"
 )
 
 func TestIntegration(t *testing.T) {
 	var (
 		projectPath             = filepath.Join(build.Default.GOPATH, "src/github.com/memprofiler/memprofiler")
 		serverCfgPathFilesystem = filepath.Join(projectPath, "server/config/example.yml")
-		//serverCfgPathTSDB       = filepath.Join(projectPath, "server/config/example_tsdb.yml")
+		serverCfgPathTSDB       = filepath.Join(projectPath, "server/config/example_tsdb.yml")
 	)
 	t.Run("filesystemStorage", testTemplate(projectPath, serverCfgPathFilesystem))
-	//t.Run("tsdbStorage", testTemplate(projectPath, serverCfgPathTSDB))
+	t.Run("tsdbStorage", testTemplate(projectPath, serverCfgPathTSDB))
 }
 
 func testTemplate(projectPath, serverConfigPath string) func(t *testing.T) {
@@ -50,21 +55,18 @@ func testTemplate(projectPath, serverConfigPath string) func(t *testing.T) {
 			t.FailNow()
 		}
 
-		log.Println("----------------------------------------------")
-		resp, err := http.Get("http://localhost:8081/v1/services")
-		if err != nil {
-			log.Println(err)
-			log.Println("----------------------Err------------------------")
-			return
-		}
-		defer resp.Body.Close()
-		body, _ := ioutil.ReadAll(resp.Body)
-		log.Println(string(body))
-		log.Println("----------------------------------------------")
-
 		// there should be the only service right now
 		assert.Len(t, getServicesResponse.ServiceTypes, 1)
 		assert.Equal(t, expectedServiceType, getServicesResponse.ServiceTypes[0])
+
+		// 1.1 validate http
+		data, err := sendHTTPReq(e.serverCfg, "services")
+		require.NoError(t, err)
+
+		httpGetServicesResponse := &schema.GetServicesResponse{}
+		err = jsonpb.Unmarshal(data, httpGetServicesResponse)
+		require.NoError(t, err)
+		require.True(t, proto.Equal(httpGetServicesResponse, getServicesResponse))
 
 		// 2. ask for list of service instances
 		getInstancesRequest := &schema.GetInstancesRequest{ServiceType: expectedServiceType}
@@ -75,6 +77,15 @@ func testTemplate(projectPath, serverConfigPath string) func(t *testing.T) {
 		// there should be the only instance right now
 		assert.Len(t, getInstancesResponse.ServiceInstances, 1)
 		assert.Equal(t, expectedServiceInstance, getServicesResponse.ServiceTypes[0])
+
+		// 2.1 validate http
+		data, err = sendHTTPReq(e.serverCfg, "instances")
+		require.NoError(t, err)
+
+		httpGetInstancesResponse := &schema.GetInstancesResponse{}
+		err = jsonpb.Unmarshal(data, httpGetInstancesResponse)
+		require.NoError(t, err)
+		require.True(t, proto.Equal(httpGetInstancesResponse, getInstancesResponse))
 
 		// 3. ask for list of sessions
 		getSessionsRequest := &schema.GetSessionsRequest{
@@ -104,6 +115,15 @@ func testTemplate(projectPath, serverConfigPath string) func(t *testing.T) {
 		// session is still operational, so the finish time is nil
 		assert.Nil(t, session.Metadata.FinishedAt)
 
+		// 3.1 validate http
+		data, err = sendHTTPReq(e.serverCfg, "sessions")
+		require.NoError(t, err)
+
+		httpGetSessionResponse := &schema.GetSessionsResponse{}
+		err = jsonpb.Unmarshal(data, httpGetSessionResponse)
+		require.NoError(t, err)
+		require.True(t, proto.Equal(httpGetSessionResponse, getSessionResponse))
+
 		// 4. subscribe for session updates
 		subscriptionRequest := &schema.SubscribeForSessionRequest{
 			SessionDescription: session.Description,
@@ -117,11 +137,26 @@ func testTemplate(projectPath, serverConfigPath string) func(t *testing.T) {
 			t.FailNow()
 		}
 
-		time.Sleep(60 * time.Second)
 		assert.NotNil(t, metrics)
 		err = subscription.CloseSend()
 		if !assert.NoError(t, err) {
 			t.FailNow()
 		}
 	}
+}
+
+func sendHTTPReq(cfg *config.Config, req string) (io.ReadCloser, error) {
+	u, err := url.Parse("http://" + cfg.Frontend.FrontendEndpoint)
+	if err != nil {
+		return nil, err
+	}
+
+	u.Path = path.Join(u.Path, "v1", req)
+
+	resp, err := http.Get(u.String())
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Body, nil
 }
